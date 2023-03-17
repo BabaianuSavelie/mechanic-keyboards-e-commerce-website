d2eab7360d387762bbb5489472bbacdf9192b547
BabaianuSavelie <babaianu.savelie1@gmail.com>
2023-03-17 15:45:49 +0200

    Add hero section slider

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -1,12 +1,13 @@
 import { Container, Image } from "@chakra-ui/react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Autoplay } from "swiper";
+import { Navigation, Autoplay, Pagination } from "swiper";
 import Image1 from "../assets/keycaps.webp";
 import Image2 from "../assets/slide1.webp";
 import Image3 from "../assets/slide2.jpg";
 
 import "swiper/css";
 import "swiper/css/navigation";
+import "swiper/css/pagination";
 import "../styles/style.css";
 
 const HeroSlider = () => {
@@ -14,10 +15,11 @@ const HeroSlider = () => {
     <Container maxW="container.2xl" mt=".8rem" borderRadius={20}>
       <Swiper
         navigation
-        modules={[Navigation, Autoplay]}
         centeredSlides={true}
         autoplay={{ delay: 2500 }}
         slidesPerView={1}
+        pagination={{ dynamicBullets: true }}
+        modules={[Navigation, Autoplay, Pagination]}
       >
         <SwiperSlide>
           <Image src={Image1} objectFit="cover" w="100%" h="100%" />
